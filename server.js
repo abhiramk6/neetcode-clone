fb20e72c545ce7a11ad139d8783fb625a628532c
abhiramk6 <2010080001@klh.edu.in>
2023-02-26 11:00:22 +0530

    Serving HTML file from express

--- server.js
+++ server.js
@@ -2,6 +2,19 @@ const express = require("express"); //import package
 
 const app = express(); // create a express app
 
+app.use(express.static("frontend")) // telling our app to use the frontend directory for static files
+
+app.get('/html', function (req, res) { // on giving path as '/html' in url this will be executed 
+
+    // __dirname will give location of current file
+
+    res.sendFile(__dirname+"/frontend/index.html"); // it gives root path we will add location to file SimpleHtml which will run 
+
+    // res.send will send string response 
+    // res.sendFile will send file from given location as response which will run
+    
+})
+
 app.get("/",function(req,res){  // on giving path as '/' in url this will be executed 
     res.send("Hello World!");
 })
@@ -10,10 +23,12 @@ app.get("/myname",function(req,res){  // on giving path as '/myname' your name w
     res.send("Abhiram kamini");
 })
 
-app.listen(3000,function(){     // giving 3000 port and function to run 
-    console.log("Servewr running on http://localhost:3000")  //using the port number 3000 we will run our server 
+app.listen(3000,function(){     //using the port number 3000 we will run our server 
+    console.log("Server running on http://localhost:3000")  // command to display when our server is running 
 });
 
 // comand line we will type 'npm start' to start server 
 // after every change restart server 
+
+
 
